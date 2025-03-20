b98480058bf875619cc9cf87fab6a5c3ee799dd2
Ilya Benikov <ilya.benikov@gmail.com>
2025-03-21 03:19:00 +0400

    init

---
+++ Animax/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Animax
+//
+//  Created by Илья Беников on 21.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
---
+++ AnimaxTests/AnimaxTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnimaxTests.swift
+//  AnimaxTests
+//
+//  Created by Илья Беников on 21.03.25.
+//
+
+import Testing
+@testable import Animax
+
+struct AnimaxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
