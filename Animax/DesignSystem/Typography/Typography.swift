57183372afb2df4bea6929bbd77c2a74cb80b10e
Ilya Benikov <ilya.benikov@gmail.com>
2025-04-12 20:04:25 +0400

    Добавил шрифты

---
+++ Animax/DesignSystem/Typography/Typography.swift
@@ -0,0 +1,48 @@
+//  Created by Илья Беников on 12.04.25.
+
+import UIKit
+
+extension UIFont {
+    struct Heading {
+        static let heading1 = UIFont(name: "Urbanist-Bold", size: 48)
+        static let heading2 = UIFont(name: "Urbanist-Bold", size: 40)
+        static let heading3 = UIFont(name: "Urbanist-Bold", size: 32)
+        static let heading4 = UIFont(name: "Urbanist-Bold", size: 24)
+        static let heading5 = UIFont(name: "Urbanist-Bold", size: 20)
+        static let heading6 = UIFont(name: "Urbanist-Bold", size: 18)
+    }
+    
+    struct Body {
+        struct Regular {
+            static let xLarge = UIFont(name: "Urbanist-Regular", size: 18)
+            static let large = UIFont(name: "Urbanist-Regular", size: 16)
+            static let medium = UIFont(name: "Urbanist-Regular", size: 14)
+            static let small = UIFont(name: "Urbanist-Regular", size: 12)
+            static let xSmall = UIFont(name: "Urbanist-Regular", size: 10)
+        }
+        
+        struct Medium {
+            static let xLarge = UIFont(name: "Urbanist-Medium", size: 18)
+            static let large = UIFont(name: "Urbanist-Medium", size: 16)
+            static let medium = UIFont(name: "Urbanist-Medium", size: 14)
+            static let small = UIFont(name: "Urbanist-Medium", size: 12)
+            static let xSmall = UIFont(name: "Urbanist-Medium", size: 10)
+        }
+        
+        struct Semibold {
+            static let xLarge = UIFont(name: "Urbanist-Semibold", size: 18)
+            static let large = UIFont(name: "Urbanist-Semibold", size: 16)
+            static let medium = UIFont(name: "Urbanist-Semibold", size: 14)
+            static let small = UIFont(name: "Urbanist-Semibold", size: 12)
+            static let xSmall = UIFont(name: "Urbanist-Semibold", size: 10)
+        }
+        
+        struct Bold {
+            static let xLarge = UIFont(name: "Urbanist-Bold", size: 18)
+            static let large = UIFont(name: "Urbanist-Bold", size: 16)
+            static let medium = UIFont(name: "Urbanist-Bold", size: 14)
+            static let small = UIFont(name: "Urbanist-Bold", size: 12)
+            static let xSmall = UIFont(name: "Urbanist-Bold", size: 10)
+        }
+    }
+}
